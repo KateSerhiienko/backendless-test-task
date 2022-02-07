1a76cd4d4541545e0a774a2d9529391a53fc98a0
KateSerhiienko <kate.serhiienko@gmail.com>
2022-02-08 00:34:21 +0200

    add(*):change theme & modal window

--- script.js
+++ script.js
@@ -1,16 +1,58 @@
-let wrapper = document.querySelector(".wrapper");
-let themes = document.getElementsByName("theme");
-let select = document.querySelector("select");
+// theme
 
-themes.forEach(theme => theme.addEventListener("click", setTheme));
-select.addEventListener('change', setTheme)
+const wrapper = document.querySelector(".wrapper");
+const themes = document.getElementsByName("theme");
+const select = document.querySelector("select");
+
+const fieldsThemes = {
+  "input": "checked",
+  "option": "selected",
+  "button": "disabled",
+};
 
 function setTheme(event) {
   removeTheme();
   wrapper.classList.add(event.target.value);
+  changeTheme(event.target.value);
 }
 
 function removeTheme() {
-  console.log(wrapper.classList);
   wrapper.classList = ["wrapper"];
 }
+
+function changeTheme(eventValue) {
+  for (let fieldTheme in fieldsThemes) {
+    let fieldsList = document.querySelectorAll(fieldTheme);
+    fieldsList.forEach(field => field.value === eventValue ? field[fieldsThemes[fieldTheme]] = true : field[fieldsThemes[fieldTheme]] = false);
+  }
+}
+
+themes.forEach(theme => theme.addEventListener("click", setTheme));
+select.addEventListener('change', setTheme)
+
+
+
+// modal
+
+const images = document.querySelectorAll(".image");
+const modalWrapper = document.querySelector(".modal-wrapper");
+const modalImage = document.querySelector(".modal-image");
+const modalClose = document.querySelector(".modal-close");
+
+modalWrapper.addEventListener('click', function (e) {
+  e.stopPropagation();
+  return false;
+})
+
+function openModal() {
+  modalWrapper.classList.add("opened");
+  modalImage.src = this.src;
+  document.body.style.overflow = 'hidden';
+}
+images.forEach(image => image.addEventListener("click", openModal));
+
+function closeModal() {
+  modalWrapper.classList.remove("opened");
+  document.body.style.overflow = '';
+}
+modalClose.addEventListener("click", closeModal);
